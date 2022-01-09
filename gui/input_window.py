e3a4afc816bf72f08304f8a9475ea07e6c9a6a8b
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-01-09 20:32:55 +0100

    added UI methods to input_window.py

--- gui/input_window.py
+++ gui/input_window.py
@@ -47,6 +47,7 @@ class InputWindow(BaseWindow):
 
         self.leagues: list[LeagueListItem] = list()
         self.teams: list[list[TeamListItem]] = list()
+        self._team_edit: bool = False
 
         self._set_window_information()
         self._create_initial_ui()
@@ -57,35 +58,83 @@ class InputWindow(BaseWindow):
         # Left
         self._league_lb = QLabel()
         self._league_list = QListWidget()
+        self._league_list.itemClicked.connect(self._set_current_league)
 
         # Right
         self._league_name_lb = QLabel()
         self._league_name_le = QLineEdit()
+        self._league_name_le.textChanged.connect(self._edit_current_league_name)
 
         self._active_cb = QCheckBox()
         self._active_cb.setChecked(True)
+        self._active_cb.toggled.connect(self._edit_current_league_active)
         self._second_round_cb = QCheckBox()
         self._second_round_cb.setChecked(True)
+        self._second_round_cb.toggled.connect(self._edit_current_league_second_round)
 
         self._team_name_lb = QLabel()
         self._team_name_le = QLineEdit()
 
         self._add_team_btn = QPushButton()
+        self._add_team_btn.pressed.connect(self._add_team_to_current_league)
         self._remove_team_btn = QPushButton()
+        self._remove_team_btn.pressed.connect(self._remove_team_from_current_league)
         self._remove_all_teams_btn = QPushButton()
+        self._remove_all_teams_btn.pressed.connect(self._remove_all_teams_from_current_league)
 
         self._teams_list_lb = QLabel()
         self._teams_list = QListWidget()
+        self._teams_list.itemClicked.connect(self._set_current_team)
 
         # Bottom
         self._remove_all_leagues_btn = QPushButton()
+        self._remove_all_leagues_btn.pressed.connect(self._remove_all_leagues)
         self._remove_league_btn = QPushButton()
+        self._remove_league_btn.pressed.connect(self._remove_league)
         self._add_league_btn = QPushButton()
+        self._add_league_btn.pressed.connect(self._add_league)
 
         self._start_btn = QPushButton()
+        self._start_btn.pressed.connect(self._start)
 
     def _set_window_information(self):
         self.setWindowTitle("KickerRechner by Purpur Tentakel // Input Window")
+
+    def _add_league(self):
+        pass
+
+    def _add_team_to_current_league(self):
+        pass
+
+    def _edit_current_league_name(self):
+        pass
+
+    def _edit_current_league_active(self):
+        pass
+
+    def _edit_current_league_second_round(self):
+        pass
+
+    def _remove_league(self):
+        pass
+
+    def _remove_all_leagues(self):
+        pass
+
+    def _remove_team_from_current_league(self):
+        pass
+
+    def _remove_all_teams_from_current_league(self):
+        pass
+
+    def _set_current_league(self):
+        pass
+
+    def _set_current_team(self):
+        pass
+
+    def _start(self):
+        pass
 
 
 window = InputWindow
