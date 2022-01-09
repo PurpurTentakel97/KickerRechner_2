ecf26175c93714ae143ab2ff2a643e9a9c1dd4d8
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-01-09 21:11:27 +0100

    added initial text to input_window.py added initial league to input_window.py

--- gui/input_window.py
+++ gui/input_window.py
@@ -1,6 +1,7 @@
 # Purpur Tentakel
 # 09.01.2022
 # KickerRechner // Input Window
+
 import sys
 
 from PyQt5.QtWidgets import QListWidget, QListWidgetItem, QApplication, QLabel, QLineEdit, QCheckBox, QPushButton
@@ -8,19 +9,24 @@ from gui.base_window import BaseWindow
 
 
 class LeagueListItem(QListWidgetItem):
-    def __init__(self, name: str, index: int):
+    def __init__(self, index: int):
         super().__init__()
-        self.name: str = name
+        self.name: str = str()
         self.index: int = index
+        self.active: bool = True
+        self.second_round: bool = True
 
         self._set_text()
 
     def _set_text(self):
-        self.setText(self.name)
+        self.setText("liga " + str(self.index + 1))
 
     def update_text(self, new_name: str):
         self.name = new_name
-        self.setText(self.name)
+        if len(self.name) == 0:
+            self._set_text()
+        else:
+            self.setText(self.name)
 
     def update_index(self, index: int):
         self.index = index
@@ -47,12 +53,12 @@ class InputWindow(BaseWindow):
 
         self.leagues: list[LeagueListItem] = list()
         self.teams: list[list[TeamListItem]] = list()
-        self._team_edit: bool = False
 
         self._set_window_information()
         self._create_initial_ui()
-
-        self.show()
+        self._create_initial_team()
+        self._set_initial_text()
+        self._set_layout()
 
     def _create_initial_ui(self):
         # Left
@@ -74,12 +80,17 @@ class InputWindow(BaseWindow):
 
         self._team_name_lb = QLabel()
         self._team_name_le = QLineEdit()
+        self._edit_team_btn = QPushButton()
+        self._edit_team_btn.setEnabled(False)
 
         self._add_team_btn = QPushButton()
+        self._add_team_btn.setEnabled(False)
         self._add_team_btn.pressed.connect(self._add_team_to_current_league)
         self._remove_team_btn = QPushButton()
+        self._remove_team_btn.setEnabled(False)
         self._remove_team_btn.pressed.connect(self._remove_team_from_current_league)
         self._remove_all_teams_btn = QPushButton()
+        self._remove_all_teams_btn.setEnabled(False)
         self._remove_all_teams_btn.pressed.connect(self._remove_all_teams_from_current_league)
 
         self._teams_list_lb = QLabel()
@@ -95,7 +106,47 @@ class InputWindow(BaseWindow):
         self._add_league_btn.pressed.connect(self._add_league)
 
         self._start_btn = QPushButton()
+        self._start_btn.setEnabled(False)
         self._start_btn.pressed.connect(self._start)
+
+    def _crate_initial_league(self):
+        league = LeagueListItem(len(self.leagues) + 1)
+        self._league_list.addItem(league)
+        self._league_list.setCurrentItem(league)
+        self.leagues.append(league)
+        self.teams.append(list())
+
+    def _set_initial_text(self):
+        current_league: LeagueListItem = self._league_list.currentItem()
+        # Left
+        self._league_lb.setText("Ligen:")
+
+        # Right
+        self._league_name_lb.setText("Liagename:")
+        self._league_name_le.setPlaceholderText("Liga" + str(current_league.index + 1))
+
+        self._active_cb.setText("Aktiv")
+        self._second_round_cb.setText("Aktiv")
+
+        self._team_name_lb.setText("Teamname:")
+        self._team_name_le.setPlaceholderText("Neues Team")
+        self._edit_team_btn.setText("Teamname bearbeiten")
+
+        self._add_team_btn.setText("Team hinzufügen")
+        self._remove_team_btn.setText("Team entfernen")
+        self._remove_all_teams_btn.setText("Alle Teams entfernen")
+
+        self._teams_list_lb.setText("Teams:")
+
+        # Bottom
+        self._remove_all_leagues_btn.setText("Alle Ligen entfernen")
+        self._remove_league_btn.setText("Liga entfernen")
+        self._add_league_btn.setText("Liga hinzufügen")
+        self._start_btn.setText("Turnier starten")
+
+    def _set_layout(self):
+
+        self.show()
 
     def _set_window_information(self):
         self.setWindowTitle("KickerRechner by Purpur Tentakel // Input Window")
