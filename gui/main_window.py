8a38dd8552b2ba482c63876f8984b95fa73d5f80
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-01-15 21:45:58 +0100

    main_window.py changed game_tabs to first_round_btn and second_round_btn

--- gui/main_window.py
+++ gui/main_window.py
@@ -76,36 +76,31 @@ class MainWindow(BaseWindow):
         self.widget = QWidget()
 
         # Top Left
-        self._league_lb = QLabel()
-        self._league_list = QListWidget()
+        self._league_lb: QLabel = QLabel()
+        self._league_list: QListWidget = QListWidget()
 
         # Top Right games
-        self._game_tabs = QTabWidget()
-        self._first_day_game_hbox = QHBoxLayout()  # Hinrunde
-        self._second_day_game_hbox = QHBoxLayout()  # Rückrunde
-        self._add_tabs_to_game_tabs()
+        self._first_round_btn: QPushButton = QPushButton()
+        self._second_round_btn: QPushButton = QPushButton()
 
-        self._day_list = QListWidget()
-        self._game_list = QListWidget()
-        self._dummy_day_list = QListWidget()
-
-        self._dummy_game_list = QListWidget()
+        self._day_list: QListWidget = QListWidget()
+        self._game_list: QListWidget = QListWidget()
 
         # Top Right next game
-        self._next_game_lb = QLabel()
-        self._team_1_lb = QLabel()
-        self._team_2_lb = QLabel()
-        self._score_1_le = QLineEdit()
-        self._score_2_le = QLineEdit()
-        self._add_score_btn = QPushButton()
+        self._next_game_lb: QLabel = QLabel()
+        self._team_1_lb: QLabel = QLabel()
+        self._team_2_lb: QLabel = QLabel()
+        self._score_1_le: QLineEdit = QLineEdit()
+        self._score_2_le: QLineEdit = QLineEdit()
+        self._add_score_btn: QPushButton = QPushButton()
         self._add_score_btn.setEnabled(False)
 
         # Bottom
-        self._table_lb = QLabel()
-        self._table_tabs = QTabWidget()
-        self._first_round_table = QTableWidget()
-        self._second_round_table = QTableWidget()
-        self._total_table = QTableWidget()
+        self._table_lb: QLabel = QLabel()
+        self._table_tabs: QTabWidget = QTabWidget()
+        self._first_round_table: QTableWidget = QTableWidget()
+        self._second_round_table: QTableWidget = QTableWidget()
+        self._total_table: QTableWidget = QTableWidget()
         self._add_tabs_to_table_tabs()
 
     def _create_initial_text(self) -> None:
@@ -113,7 +108,8 @@ class MainWindow(BaseWindow):
         self._league_lb.setText("Ligen:")
 
         # Top Right games
-        # no initial text
+        self._first_round_btn.setText("Hinrunde")
+        self._second_round_btn.setText("Rückrunde")
 
         # Top Right next game
         self._next_game_lb.setText("Nächstes Spiel:")
@@ -121,54 +117,58 @@ class MainWindow(BaseWindow):
 
     def _create_initial_layout(self) -> None:
         # Top Left
-        league_lb_hbox = QHBoxLayout()
+        league_lb_hbox: QHBoxLayout = QHBoxLayout()
         league_lb_hbox.addWidget(self._league_lb)
         league_lb_hbox.addStretch()
 
-        league_vbox = QVBoxLayout()
+        league_vbox: QVBoxLayout = QVBoxLayout()
         league_vbox.addLayout(league_lb_hbox)
         league_vbox.addWidget(self._league_list)
 
         # Top Right games
-        # game_tabs
         # created in UI
-        self._first_day_game_hbox.addWidget(self._day_list)
-        self._first_day_game_hbox.addWidget(self._game_list)
+        games_hbox: QHBoxLayout = QHBoxLayout()
+        games_hbox.addWidget(self._first_round_btn)
+        games_hbox.addWidget(self._second_round_btn)
+        games_hbox.addStretch()
 
-        # created in UI
-        self._second_day_game_hbox.addWidget(self._dummy_day_list)
-        self._second_day_game_hbox.addWidget(self._dummy_game_list)
+        games_list_hbox: QHBoxLayout = QHBoxLayout()
+        games_list_hbox.addWidget(self._day_list)
+        games_list_hbox.addWidget(self._game_list)
+
+        games_vbox: QVBoxLayout = QVBoxLayout()
+        games_vbox.addLayout(games_hbox)
+        games_vbox.addLayout(games_list_hbox)
 
         # Top Right next game
-        next_game_lb_hbox = QHBoxLayout()
+        next_game_lb_hbox: QHBoxLayout = QHBoxLayout()
         next_game_lb_hbox.addWidget(self._next_game_lb)
         next_game_lb_hbox.addStretch()
 
-        add_game_gl = QGridLayout()
+        add_game_gl: QGridLayout = QGridLayout()
         add_game_gl.addWidget(self._team_1_lb, 0, 0)
-
         add_game_gl.addWidget(QLabel("gegen"), 1, 0)
         add_game_gl.addWidget(self._team_2_lb, 2, 0)
         add_game_gl.addWidget(self._score_1_le, 0, 1)
         add_game_gl.addWidget(self._score_2_le, 2, 1)
         add_game_gl.addWidget(self._add_score_btn, 2, 2)
 
-        add_game_hbox = QHBoxLayout()
+        add_game_hbox: QHBoxLayout = QHBoxLayout()
         add_game_hbox.addLayout(add_game_gl)
         add_game_hbox.addStretch()
 
-        top_right_vbox = QVBoxLayout()
-        top_right_vbox.addWidget(self._game_tabs)
+        top_right_vbox: QVBoxLayout = QVBoxLayout()
+        top_right_vbox.addLayout(games_vbox)
         top_right_vbox.addLayout(next_game_lb_hbox)
         top_right_vbox.addLayout(add_game_hbox)
 
         # Top
-        top_hbox = QHBoxLayout()
+        top_hbox: QHBoxLayout = QHBoxLayout()
         top_hbox.addLayout(league_vbox)
         top_hbox.addLayout(top_right_vbox)
 
         # Global
-        global_vbox = QVBoxLayout()
+        global_vbox: QVBoxLayout = QVBoxLayout()
         global_vbox.addLayout(top_hbox)
         global_vbox.addWidget(self._table_tabs)
 
@@ -188,14 +188,6 @@ class MainWindow(BaseWindow):
                 else:
                     league_item.second_round_games.append(game)
         self._league_list.setCurrentItem(self._league_list_items[self._next_league_index])
-
-    def _add_tabs_to_game_tabs(self) -> None:
-        first_widget = QWidget()
-        first_widget.setLayout(self._first_day_game_hbox)
-        self._game_tabs.addTab(first_widget, "Hinrunde")
-        second_widget = QWidget()
-        second_widget.setLayout(self._second_day_game_hbox)
-        self._game_tabs.addTab(second_widget, "Rückrunde")
 
     def _add_tabs_to_table_tabs(self) -> None:
         self._table_tabs.addTab(self._first_round_table, "Hinrunde")
